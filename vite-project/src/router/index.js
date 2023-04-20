b975bdf7af7dd09d4ca56665cb90db943800a9b8
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-04-20 10:08:16 -0400

    chart js

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -3,8 +3,8 @@ import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "About", component: About },
+  { path: "/", name: "Home", component: About },
+  { path: "/about", name: "About", component: Home },
 ];
 const router = createRouter({
   history: createWebHistory(),
