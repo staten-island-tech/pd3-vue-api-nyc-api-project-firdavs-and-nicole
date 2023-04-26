7e2be69fef750714a5f994da79153ada16009436
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-04-26 10:11:33 -0400

    i try to do as mr whalen asks

--- vite-project/src/views/About.vue
+++ vite-project/src/views/About.vue
@@ -1,44 +1,54 @@
+<script setup></script>
+
 <template>
   <div class="about">
     <h1>About</h1>
   </div>
-  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <Doughnut
+    id="my-chart-id"
+    v-if="loaded"
+    :options="chartOptions"
+    :data="chartData"
+  />
+  <button @click="getpost">ggg</button>
 </template>
-
 <script>
-import { Bar } from "vue-chartjs";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
+import { Doughnut } from "vue-chartjs";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default {
   name: "BarChart",
-  components: { Bar },
+  components: { Doughnut },
   data() {
     return {
-      chartData: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
-      },
-      chartOptions: {
-        responsive: true,
-      },
+      chartData: false,
+      chartOptions: null,
+      responsive: false,
     };
+  },
+  mountChart: function () {
+    let x = ["40", "60"];
+    this.chartData = {
+      labels: ["Pitbulls", "Anything Else", "Cats"],
+      datasets: [{ backgroundColor: ["blue,green"], data: this.x }],
+    };
+    this.chartOptions = {
+      responsive: true,
+    };
+    this.loaded = true;
+  },
+  methods: {
+    getpost: async function () {
+      const response = await fetch(
+        "https://data.cityofnewyork.us/resource/rsgh-akpg.json"
+      );
+      const dogs = await response.json();
+      dogs.forEach((e) => {
+        console.log(e.breed);
+      });
+    },
   },
 };
 </script>
